deb99243ea770f1cd69b5a5cb92f8688d86e2db3
Pecapilli <edoardo97@gmail.com>
2020-04-01 20:16:22 +0200

    Create data_output.php

---
+++ cappelli_edoardo/data_output.php
@@ -0,0 +1,24 @@
+<?php
+
+$prendi_nome = $_GET["nome_utente"];
+$prendi_sesso = $_GET["sesso"];
+$prendi_messaggio = $_GET["messaggio"];
+$prendi_destinatario = $_GET["destinatario"];
+/*
+$messaggio = $_GET["messaggio"];
+$sesso = $_GET["sesso"];
+$interessi = $_GET["interessi"];
+*/
+
+?>
+
+<h2>La risposta</h2>
+
+Il tuo nome è: <?=$prendi_nome?>
+<br>
+Il tuo sesso è: <?=$prendi_sesso?>
+<br>
+Il messaggio inviato è: <?=$prendi_messaggio?>
+<br>
+Il destinatario è: <?=$prendi_destinatario?>
+
