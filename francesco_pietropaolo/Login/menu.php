437299c2ad5253da9b6669e4686f200d30b540e1
Pietropaolo <f.pietropaolo96@gmail.com>
2020-04-06 11:20:41 +0200

    Esercizio per lunedì

--- francesco_pietropaolo/Login/menu.php
+++ francesco_pietropaolo/Login/menu.php
@@ -1,11 +1,11 @@
 <ul>
-  <li>
+  <li class="loginpage">
     <a href="3-sessione.php">LOGIN FORM</a>
   </li>
-  <li>
+  <li class="homepage">
     <a href="home.php">home</a>
   </li>
-  <li>
+  <li class="logoutpage">
     <a href="logout.php">Logout</a>
   </li>
 </ul>
