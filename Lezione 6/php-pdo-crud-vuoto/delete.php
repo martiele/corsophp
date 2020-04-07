5081efc49e93f8fe8d82675257a95f6a056423ea
Daniele Martini <ing.martini@gmail.com>
2020-04-07 13:02:26 +0200

    delete

--- Lezione 6/php-pdo-crud-vuoto/delete.php
+++ Lezione 6/php-pdo-crud-vuoto/delete.php
@@ -1,3 +1,24 @@
 <?php
+
+require_once("db.php");
+
+
 	//Codice eliminazione record (e redirect a index.php)
+if( isset($_GET) && isset($_GET["id"]) && ($_GET["id"]>0)  ){
+
+	$id_to_delete = $_GET["id"];
+	//$query = "DELETE FROM `posts` WHERE `posts`.`id` = " . $id_to_delete;
+	$query = "DELETE FROM `posts` WHERE `posts`.`id` = $id_to_delete";
+
+	$result = $conn->prepare($query);
+	$result->execute();
+
+	//a questo punto ho eliminato il record dal db
+
+}
+
+header("location:index.php");
+exit();
+
+
 ?>
