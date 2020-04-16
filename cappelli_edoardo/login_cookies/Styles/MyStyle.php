3ecb563108c961c40cf55edb29c67ce319cd92db
Pecapilli <edoardo97@gmail.com>
2020-04-16 12:12:03 +0200

    Create MyStyle.php

---
+++ cappelli_edoardo/login_cookies/Styles/MyStyle.php
@@ -0,0 +1,5 @@
+<link type="text/css" rel="stylesheet" href="Styles/Bundle.css" />
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script type="text/javascript" src="Scripts/JavaScript/Bundle.js"></script>
